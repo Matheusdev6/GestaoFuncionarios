3c1f22139351299d3348213e902ed57303bc90d2
Matheus <m.dalanora.masson2@gmail.com>
2026-02-25 20:19:24 -0300

    first commit

---
+++ src/test/java/ProjetosMatheus/Gestao/GestaoFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjetosMatheus.Gestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
