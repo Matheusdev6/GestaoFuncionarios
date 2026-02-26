2f3b976d4caf82122a0d7523c8f185a2a568231f
Matheus <m.dalanora.masson2@gmail.com>
2026-02-26 10:19:35 -0300

    Controller criado.

---
+++ src/main/java/ProjetosMatheus/Gestao/Controller.java
@@ -0,0 +1,13 @@
+package ProjetosMatheus.Gestao;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "Boas vindas";
+    }
+}
